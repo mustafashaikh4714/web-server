293a138c05ea8115307ea36cad5269da0cf1dedc
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2018-10-15 21:29:26 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express'),
+hbs = require('hbs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use((req ,res, next) => {
+  var date = new Date().toString();
+  console.log(`${date}: ${req.method}, ${req.url}`);
+   next(); 
+});
+
+// app.use(express.static(__dirname +'/public'));
+
+app.get('/',(req , res) => {
+    res.render('home.hbs', {
+        pageTitle : 'Home Page',
+        currentYear : new Date().getFullYear()
+    })
+});
+
+app.get('/about',(req , res) => {
+   res.render('about.hbs', {
+       pageTitle : 'About Page',
+       currentYear : new Date().getFullYear()
+   })
+});
+
+app.listen(3000, () => {
+    console.log("server is running up on the port 3000");
+});
